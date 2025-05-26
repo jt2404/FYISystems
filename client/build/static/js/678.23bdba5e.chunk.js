2db3303cedf7c1aafcfc1fec9394ac39c056ed13
jt2404 <jayhthakkar2404@gmail.com>
2025-05-26 21:08:58 +0530

    build added.

---
+++ client/build/static/js/678.23bdba5e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_minimal_minimal_kit_react=self.webpackChunk_minimal_minimal_kit_react||[]).push([[678],{19678:function(i,a,e){e.r(a);var m=e(41230);a.default=m.g}}]);
+//# sourceMappingURL=678.23bdba5e.chunk.js.map
